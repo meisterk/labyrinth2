81312463f812754f4021e133978873946b77058d
meisterk <franz.kohnle@sabel.com>
2021-03-03 11:57:36 +0100

    Add borders

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const WIDTH = 10;
 const HEIGHT = 10;
 
+class Cell {
+    constructor() {
+        this.text = '.';
+
+        this.borderTop = false;
+        this.borderRight = false;
+        this.borderLeft = false;
+        this.borderBottom = false;
+    }
+}
+
 const App = {
     data() {
         return {
@@ -12,13 +23,17 @@ const App = {
             for (let row = 0; row < HEIGHT; row++) {
                 this.board.push([]);
                 for (let column = 0; column < WIDTH; column++) {
-                    this.board[row].push('X');
+                    this.board[row].push(new Cell());
                 }
             }
+        },
+        createLabyrinth() {
+            this.board[3][5].borderBottom = true;
         }
     },
     mounted() {
         this.fillBoard();
+        this.createLabyrinth();
     }
 };
 Vue.createApp(App).mount('#app');
